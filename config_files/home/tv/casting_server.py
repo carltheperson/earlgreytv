e0136dde4d7b05ca38c5277dd47c4c672f5e8327
Carl Riis <carltheperson@protonmail.com>
2024-07-01 17:49:18 +0200

    Using extra secret from file system

--- config_files/home/tv/casting_server.py
+++ config_files/home/tv/casting_server.py
@@ -8,10 +8,17 @@ import os
 env = os.environ.copy()
 env["WAYLAND_DISPLAY"] = "wayland-1"
 
-with open('/etc/machine-id', 'r') as file:
-    machine_id = file.read().strip()
-token = hashlib.sha256(machine_id.encode()).hexdigest()
-print("Token for checksum:", token)
+MACHINE_ID_PATH = "/etc/machine-id"
+EXTRA_SECRET_PATH = "/home/tv/extra_secret.txt"
+
+with open(MACHINE_ID_PATH, 'r') as file:
+    machine_id = file.read()
+extra = ""
+if os.path.exists(EXTRA_SECRET_PATH):
+    with open(EXTRA_SECRET_PATH) as file:
+        extra = file.read()
+secret = hashlib.sha256((machine_id + extra + "earlcasting").encode()).hexdigest()
+print("Secret for checksum:", secret)
 
 def clean_url(url):
   # I want no funny business in my URLs
@@ -29,12 +36,14 @@ class CastingHandler(SimpleHTTPRequestHandler):
     checksum = self.path[1: (64 + 1)]
     url = self.path[(64 + 1):]
 
-    expected_checksum = hashlib.sha256((token + url).encode()).hexdigest()
+    expected_checksum = hashlib.sha256((secret + url).encode()).hexdigest()
     if checksum != expected_checksum:
         self.send_response(400)
         return
     url = clean_url(url) 
 
+    # Most of these delays and commands aren't needed.
+    # I have them because rare cases without delay don't work.
     command = [
         'wtype',
         '-M', 'ctrl',
@@ -51,9 +60,23 @@ class CastingHandler(SimpleHTTPRequestHandler):
         '-s', '1000',
         '-k', 'KP_Enter',
         '-s', '2500',
-        '-k', 'Escape'
+        '-k', 'Escape',
+        '-s', '1000',
+        '-M', 'ctrl',
+        '-P', 'r',
+        '-m', 'ctrl',
+        '-p', 'r',
     ]
-    sp.run(command, check=True, text=True, env=env)
+    try:
+        result = sp.run(command, check=True, text=True, capture_output=True, env=env)
+        print(result.stdout)
+        print(result.stderr)
+    except Exception as e:
+        print(e.stdout)
+        print(e.stderr)
+        self.send_response(500)
+        return
+
 
     self.send_response(200)
     self.send_header('Content-type', 'text/plain') 
